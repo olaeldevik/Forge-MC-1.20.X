bb21f8355dea5cf545525eb502c7209bf80ccc08
olaeldevik <kalieldevik@gmail.com>
2024-04-27 15:24:22 -0500

    custom blocks

---
+++ src/main/java/net/ola/olamod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.ola.olamod.item;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.ola.olamod.OlaMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, OlaMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
